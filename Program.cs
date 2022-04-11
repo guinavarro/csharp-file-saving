7f9fecafacb118662b1eabbc10cdad43d32dc102
Guilherme Navarro <guilherme-navarro@outlook.com>
2022-04-11 12:36:15 -0300

    gestao-clientes

---
+++ Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace GestaoDeClientes
+{
+    internal class Program
+    {
+        // Notação necessária para salvar os dados em arquivos
+        [System.Serializable]
+        struct Cliente
+        {
+            public string nome;
+            public string email;
+            public string cpf;
+        }
+
+        static List<Cliente> clientes = new List<Cliente>();
+
+        // Itens do Menu
+        enum Menu { Listagem = 1, Adicionar = 2, Remover = 3, Sair = 4}
+        static void Main(string[] args)
+        {
+            Carregar();
+            bool escolheuSair = false;
+            while (!escolheuSair)
+            {
+                Console.WriteLine("Sistema de Clientes - Bem vindo!");
+                Console.WriteLine("1 - Listagem\n2 - Adicionar\n3 - Remover\n4 - SAIR");
+                int intOp = int.Parse(Console.ReadLine());
+                Menu opcao = (Menu)intOp;
+
+                switch (opcao)
+                {
+                    case Menu.Listagem:
+                        Listagem();
+                        break;
+                    case Menu.Adicionar:
+                        Adicionar();
+                        break;
+                    case Menu.Remover:
+                        Remover();
+                        break;
+                    case Menu.Sair:
+                        escolheuSair = true;
+                        break;
+                }
+                Console.Clear();
+
+            }          
+            
+        }
+
+        static void Adicionar()
+        {
+            Cliente cliente = new Cliente();
+            Console.WriteLine("CADASTRO DE CLIENTE");
+            Console.WriteLine("Nome do cliente: ");
+            cliente.nome = Console.ReadLine();
+            Console.WriteLine("E-mail do cliente:");
+            cliente.email = Console.ReadLine();
+            Console.WriteLine("CPF do cliente: ");
+            cliente.cpf = Console.ReadLine();
+
+            clientes.Add(cliente);
+            Salvar();
+            Console.WriteLine("Cadastro concluído, aperte ENTER para sair.");
+            Console.ReadLine();
+        }
+
+        static void Listagem()
+        {
+            if(clientes.Count > 0) 
+            {
+                Console.WriteLine("LISTA DE CLIENTES\n");
+                int i = 0;
+                foreach (Cliente cliente in clientes)
+                {
+                    Console.WriteLine($"ID: {i}");
+                    Console.WriteLine($"Nome: {cliente.nome}");
+                    Console.WriteLine($"E-mail: {cliente.email}");
+                    Console.WriteLine($"CPF: {cliente.cpf}");
+                    Console.Write("=====================================\n");
+                    i++;
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nenhum cliente cadastrado!");
+            }
+           
+            Console.WriteLine("Aperte ENTER para sair.");
+            Console.ReadLine();
+        }
+
+        static void Remover()
+        {
+            Listagem();
+            Console.WriteLine("Digite o ID do cliente que você quer remover:");
+            int id = int.Parse(Console.ReadLine());
+            if(id >= 0 && id < clientes.Count)
+            {
+                clientes.RemoveAt(id);
+                Salvar();
+            }
+            else
+            {
+                Console.WriteLine("ID digitado é inválido... Tente novamente!");
+                Console.ReadLine();
+            }
+        }
+
+        static void Salvar()
+        {
+            FileStream stream = new FileStream("clientes.dat",FileMode.OpenOrCreate);
+            BinaryFormatter enconder = new BinaryFormatter();
+
+            enconder.Serialize(stream, clientes);
+
+            stream.Close();
+        }
+
+        static void Carregar()
+        {
+            FileStream stream = new FileStream("clientes.dat", FileMode.OpenOrCreate);
+
+            try
+            {
+                                
+                BinaryFormatter enconder = new BinaryFormatter();
+
+                clientes = (List<Cliente>)enconder.Deserialize(stream);
+
+                if(clientes == null)
+                {
+                    clientes = new List<Cliente>();
+                }            
+
+            }
+            catch(Exception e)
+            {
+                clientes = new List<Cliente>();
+            }
+
+            stream.Close();
+        }
+    }
+}
